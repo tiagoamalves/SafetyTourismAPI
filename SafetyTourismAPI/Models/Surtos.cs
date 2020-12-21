3a27d5ae7b4fa096487743a1625bc489b2c81c60
Paulo-Torcato <paulotorcato@upskill.pt>
2020-12-21 16:50:03 +0000

    Surtos

---
+++ SafetyTourismAPI/Models/Surtos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SafetyTourismAPI.Models
+{
+    public class Surtos
+    {
+        public long SurtosID { get; set; }
+        public long VirusID { get; set; }
+        public long Id { get; set; }
+
+        public DateTime DataInicio { get; set; }
+
+        public DateTime DataFim { get; set; }
+
+        public Virus Virus { get; set; }
+        public Zona Zona { get; set; }
+        public Pais pais { get; set; }
+
+    }
+}
