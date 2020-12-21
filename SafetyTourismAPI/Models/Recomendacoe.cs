2901b86033a90c5039ab54a6f77b8595a10743e3
pedrocasimiro <pedro_sc90@upskill.pt>
2020-12-21 11:47:46 +0000

    Recomendacoe

---
+++ SafetyTourismAPI/Models/Recomendacoe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SafetyTourismAPI.Models
+{
+    public class Recomendacoe
+    {
+        public long NotaId { get; set; }
+
+        [ForeignKey]
+        public Zona zona { get; set; }
+        public DateTime date { get; set; }
+        public string ValidadeNota { get; set; }
+        public string info { get; set; }
+    }
+}
