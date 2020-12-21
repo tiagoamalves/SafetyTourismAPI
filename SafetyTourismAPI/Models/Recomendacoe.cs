4f3be4f6d67ee15360879c0660e92f5ce5794ec2
pedrocasimiro <pedro_sc90@upskill.pt>
2020-12-21 12:13:30 +0000

    Rec

--- SafetyTourismAPI/Models/Recomendacoe.cs
+++ SafetyTourismAPI/Models/Recomendacoe.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SafetyTourismAPI.Models
 {
     public class Recomendacoe
     {
         public long NotaId { get; set; }
+        public string info { get; set; }
 
-        [ForeignKey]
+        [ForeignKey("CodigoZonaGeográfica")]
         public Zona zona { get; set; }
         public DateTime date { get; set; }
-        public string ValidadeNota { get; set; }
-        public string info { get; set; }
+        public int ValidadeNota { get; set; }
     }
 }
