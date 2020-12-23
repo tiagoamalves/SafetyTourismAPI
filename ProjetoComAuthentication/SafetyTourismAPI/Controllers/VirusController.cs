50fb9e95629e6ba62441cdb3cfa9a76e9c98cbc6
VicDuarte <duarte92rafc@upskill.pt>
2020-12-23 10:54:36 +0000

    update do projeto com authentication

---
+++ ProjetoComAuthentication/SafetyTourismAPI/Controllers/VirusController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SafetyTourismAPI.Models;
+
+namespace SafetyTourismAPI.Controllers
+{
+    [Route("api/Virus")]
+    [ApiController]
+    public class VirusController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public VirusController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Virus
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Virus>>> GetVirus()
+        {
+            return await _context.Virus.ToListAsync();
+        }
+
+        // GET: api/Virus/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Virus>> GetVirus(long id)
+        {
+            var virus = await _context.Virus.FindAsync(id);
+
+            if (virus == null)
+            {
+                return NotFound();
+            }
+
+            return virus;
+        }
+
+        // PUT: api/Virus/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVirus(long id, Virus virus)
+        {
+            if (id != virus.virusID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(virus).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VirusExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Virus
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Virus>> PostVirus(Virus virus)
+        {
+            Virus v = await _context.Virus.SingleOrDefaultAsync(v => v.NomeVirus == virus.NomeVirus);
+            if (v == null)
+            {
+                _context.Virus.Add(virus);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(GetVirus), new { id = virus.virusID }, virus);
+            }
+            else
+            {
+                return Conflict();
+            }
+        }
+
+        // DELETE: api/Virus/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVirus(long id)
+        {
+            var virus = await _context.Virus.FindAsync(id);
+            if (virus == null)
+            {
+                return NotFound();
+            }
+
+            _context.Virus.Remove(virus);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VirusExists(long id)
+        {
+            return _context.Virus.Any(e => e.virusID == id);
+        }
+    }
+}
---
+++ ProjetoComAuthentication/SafetyTourismAPI/Controllers/ZonasController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SafetyTourismAPI.Models;
+
+namespace SafetyTourismAPI.Controllers
+{
+    [Route("api/Zonas")]
+    [ApiController]
+    public class ZonasController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public ZonasController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Zonas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Zona>>> GetZona()
+        {
+            return await _context.Zona.ToListAsync();
+        }
+
+        // GET: api/Zonas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Zona>> GetZona(long id)
+        {
+            var zona = await _context.Zona.FindAsync(id);
+
+            if (zona == null)
+            {
+                return NotFound();
+            }
+
+            return zona;
+        }
+
+        // PUT: api/Zonas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutZona(long id, Zona zona)
+        {
+            if (id != zona.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(zona).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ZonaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Zonas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Zona>> PostZona(Zona zona)
+        {
+            Zona z = await _context.Zona.SingleOrDefaultAsync(z => z.Name == zona.Name );
+            if (z == null)
+            {
+                _context.Zona.Add(zona);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(GetZona), new { id = zona.Name}, zona);
+            }
+            else
+            {
+                return Conflict();
+            }
+        }
+
+        // DELETE: api/Zonas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteZona(long id)
+        {
+            var zona = await _context.Zona.FindAsync(id);
+            if (zona == null)
+            {
+                return NotFound();
+            }
+
+            _context.Zona.Remove(zona);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ZonaExists(long id)
+        {
+            return _context.Zona.Any(e => e.Id == id);
+        }
+    }
+}
